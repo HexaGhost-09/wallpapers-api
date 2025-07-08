ecc15cc508b160c3b863464ef4dc0734fda68462
Hexa Ghost 09 <hexaghost-09@tutamail.com>
2025-07-08 17:56:37 +0000

    New

---
+++ api.ts
@@ -0,0 +1,16 @@
+import { serveFile } from "https://deno.land/std@0.224.0/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+
+serve(async (req) => {
+  const url = new URL(req.url);
+  
+  if (url.pathname === "/categories.json") {
+    return await serveFile(req, "./categories.json");
+  }
+  
+  if (url.pathname.startsWith("/categories/")) {
+    return await serveFile(req, "." + url.pathname);
+  }
+
+  return new Response("Not Found", { status: 404 });
+});
